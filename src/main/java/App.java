407244a04abb7255e19eb9a44c1ca4a197d7a1f8
vmuchui <kaigongimuchui@gmail.com>
2019-06-16 16:10:44 +0300

    Created user interface.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,38 @@
+import java.io.Console;
 public class App {
     public static void main(String[] args) {
-        
+        Console myConsole = System.console();
+        boolean programRunning = true;
+        System.out.println("Welcome To Caesar-Cipher :-)");
+        while (programRunning) {
+            System.out.println("Would you like to : 1.encrypt, 2.decrypt 3. Exit");
+            System.out.println("Choose either 1, 2 or 3");
+            String inputChoice = myConsole.readLine();
+            if (inputChoice.equals("1")) {
+                System.out.println("Enter the word you would like to encrypt: ");
+                String inputtedName = myConsole.readLine();
+                System.out.println("Enter the key you would like to use for your encryption!");
+                int inputtedKey = Integer.parseInt(myConsole.readLine());
+                CaesarCipher createdCaesarCipher = new CaesarCipher(inputtedName, inputtedKey);
+                String encrypted = createdCaesarCipher.encrypt();
+                System.out.println("Your encryption for the word " + inputtedName + " using the key " + inputtedKey + " is: ");
+                System.out.println(encrypted);
+                System.out.println("------------------");
+            }else if (inputChoice.equals("2")) {
+                System.out.println("Enter the word you would like to decrypt: ");
+                String inputtedName = myConsole.readLine();
+                System.out.println("Enter the key to use for your decryption!");
+                int inputtedKey = Integer.parseInt(myConsole.readLine());
+                CaesarCipher createdCaesarCipher = new CaesarCipher(inputtedName, inputtedKey);
+                String decrypted = createdCaesarCipher.decrypt();
+                System.out.println("Your decryption for the word " + inputtedName + " using the key " + inputtedKey + " is: ");
+                System.out.println(decrypted);
+                System.out.println("-------------------");
+            }else if (inputChoice.equals("3")) {
+                programRunning = false;
+            }else {
+                System.out.println("Sorry your input is not recognised!! Please enter a number either 1, 2 or 3");
+            }
+        }
     }
 }
